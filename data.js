c725b9f4e265796dcbd3b260ccf3a37e9dbe684b
Mariana V <marianvj17@gmail.com>
2022-08-16 20:18:14 -0500

    Pequeño cambio

--- data.js
+++ data.js
@@ -1,4 +1,7 @@
 //* Ejemplo 2
+// Intente con ejemplos de internet para ver si era mi terminal o yo la que nomas no da una. 
+//Pero ya no se que sea en todos me da error
+
 function getData(value) {
     if (value > 1) {
        return true;
